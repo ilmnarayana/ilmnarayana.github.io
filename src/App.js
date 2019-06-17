669168c80000e1062fe94b88a20869223d1c1883
Manikanta Narayana <ilmnarayana@gmail.com>
2019-06-17 15:04:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import SimpleCounter from './components/SimpleCounter';
+import Login from './components/Login';
+import Forms from './components/Forms';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <MainContent/>
+      <SimpleCounter/>
+      <Login/>
+      <Forms/>
+      <Footer/>
     </div>
   );
 }
